ff65d92a3e8612be145c145489e0c401168e1f23
AchRef <rahaliashraf732@gmail.com>
2022-12-29 18:19:45 +0100

    done

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -1,13 +1,14 @@
-from rest_framework import Response
-from rest_framework import APIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from .pusher import pusher_client
 
 
 class MessageAPIView(APIView):
+
     def post(self, request):
         pusher_client.trigger('chat', 'message', {
             'username': request.data['username'],
-            'message': request.data['message']
+            'message': request.data['message'],
         })
 
         return Response([])
